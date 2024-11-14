f1b4ca2277ad356e017d9b50180b58260592b1a3
collins <collins.lemogo-kamnen@student.junia.com>
2024-11-14 21:45:26 +0100

    copie des fichiers de App

--- m1-site/m1-site/.next/types/app/books/page.ts
+++ m1-site/m1-site/.next/types/app/books/page.ts
@@ -1,8 +1,8 @@
 // File: C:\Users\bensk\Desktop\finalgithub\m1-site\m1-site\src\app\books\page.tsx
-import * as entry from '../../../../src/app/books/page.js'
+import * as entry from '../../../../src/app/books/page.jsx'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../src/app/books/page.js')
+type TEntry = typeof import('../../../../src/app/books/page.jsx')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
